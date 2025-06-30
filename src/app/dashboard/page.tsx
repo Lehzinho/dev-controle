3fd570b622915c9105f0d36281ebe853df966137
Alexandre Toulios <99737712+Lehzinho@users.noreply.github.com>
2025-06-30 19:01:25 -0300

    feat: adds `dashboard` page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -10,7 +10,7 @@ export default async function Dashboard() {
         <div className="flex items-center justify-between">
           <h1 className="text-3xl font-bold">Chamados</h1>
           <Link
-            href="/dashboard/new"
+            href="/dashboard/customer/new"
             className="bg-blue-500 px-4 py-1 rounded text-white"
           >
             Abrir chamado
